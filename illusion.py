71b8d6c1177f7402b7848b7bb429a49ad85531fe
Peyton <66343323+Peyton-C@users.noreply.github.com>
2026-07-31 01:54:08 -0230

    Add defer for info commands, hopefully lowers chance of discord complaining about the delay

--- illusion.py
+++ illusion.py
@@ -753,18 +753,19 @@ async def increase(interaction: discord.Interaction, sku: str, amount: str | Non
 @bot.tree.command(name="info", description="Get info about an item")
 @app_commands.describe(sku="Item Sku", hide_ext="Show or hide extra values")
 async def info(interaction: discord.Interaction, sku: str, hide_ext: bool = True):
+    await interaction.response.defer()
     response_message = await command_handler.handler_info(sku, hide_ext)
 
     if response_message.startswith("Invalid sku"):
-        await interaction.response.send_message(response_message)
+        await interaction.followup.send(response_message)
     else:
         cleaned_sku = illusion_helpers.clean_sku(sku)
         item = inventory.get_item(cleaned_sku)
 
         if len(response_message) <= 2000:
-            await interaction.response.send_message(f"```{response_message}```", view=illusion_helpers.make_vendor_buttons(item),)
+            await interaction.followup.send(f"```{response_message}```", view=illusion_helpers.make_vendor_buttons(item),)
         else:
-            await interaction.response.send_message(f"I didnt feel like handling info lookups with > 2000 chars, if thix happens from a real item, please ping me -PC")
+            await interaction.followup.send(f"I didnt feel like handling info lookups with > 2000 chars, if thix happens from a real item, please ping me -PC")
 
 @bot.tree.command(name="delete", description="Delete an item")
 @app_commands.describe(sku="Item Sku")
